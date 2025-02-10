a3b69fb40760550a609cf448c8d6718cc747bf65
JungHm <taggma12@gmail.com>
2025-02-10 13:59:47 +0900

    SwiftUI First Commit

---
+++ SwiftUI-FirstDay/SwiftUI-FirstDay/SwiftUI_FirstDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FirstDayApp.swift
+//  SwiftUI-FirstDay
+//
+//  Created by 안정흠 on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FirstDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
