9b22bb69eb76d603a29b73678628c5f8df5eb4b8
JungHm <taggma12@gmail.com>
2025-02-13 15:34:07 +0900

    TextField 예제 추가

---
+++ SwiftUI-FirstDay/SwiftUI-FirstDay/20250211/PickerExample.swift
@@ -0,0 +1,18 @@
+//
+//  PickerExample.swift
+//  JSONDecodeTest
+//
+//  Created by 안정흠 on 2/13/25.
+//
+
+import SwiftUI
+
+struct PickerExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PickerExample()
+}
