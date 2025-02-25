c28a91dfa9c82940fba670d3bb987a5bffa0ce5a
JungHm <taggma12@gmail.com>
2025-02-25 11:51:35 +0900

    스토리보드, 뷰컨트롤러 연결

--- EST-UIKit/EST-UIKit/ViewController.swift
+++ EST-UIKit/EST-UIKit/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var nameField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Tap \(sender.titleLabel?.text)")
+        if sender == resetButton {
+            print("리셋 버튼 탭!")
+        }
+        else {
+            print("버튼이 sender인 경우")
+        }
+    }
 }
 
