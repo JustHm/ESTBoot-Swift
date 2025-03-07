1fb1b1c9fdf179561dc9d2a0ccb1acba3e693508
JungHm <taggma12@gmail.com>
2025-03-07 23:08:27 +0900

    Feat: 메모뷰 (TextContentViewController) UI 추가

--- Assignment/WeeklyProject-MyContentManager/WeeklyProject-MyContentManager/Presentation/Memo/TextContentViewController.swift
+++ Assignment/WeeklyProject-MyContentManager/WeeklyProject-MyContentManager/Presentation/Memo/TextContentViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class TextContentViewController: UIViewController {
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+    private let emptyView = EmptyGuideView(
+        systemImage: UIImage(systemName: "text.document"),
+        title: "메모가 없습니다.",
+        message: "오른쪽 위 "+" 버튼을 눌러 메모를 추가하세요"
+    )
+    private let floatingButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "plus"), for: .normal)
+        button.tintColor = .white
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
