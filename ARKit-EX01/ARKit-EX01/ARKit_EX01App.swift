5d09a8152474b118ebe2e678d54491cd51c2dfcb
JungHm <taggma12@gmail.com>
2025-04-25 10:13:23 +0900

    ARKit 프로젝트 생성

---
+++ ARKit-EX01/ARKit-EX01/ARKit_EX01App.swift
@@ -0,0 +1,17 @@
+//
+//  ARKit_EX01App.swift
+//  ARKit-EX01
+//
+//  Created by 안정흠 on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ARKit_EX01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
