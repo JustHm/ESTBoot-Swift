ebddc1af0813bdde6481200aab7df448f0d1b5b8
JungHm <taggma12@gmail.com>
2025-04-28 16:48:52 +0900

    ARKit 미니프로젝트 생성

---
+++ ARKit-MINI/ARKit-MINI/ARKit_MINIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKit_MINIApp.swift
+//  ARKit-MINI
+//
+//  Created by 안정흠 on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ARKit_MINIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
